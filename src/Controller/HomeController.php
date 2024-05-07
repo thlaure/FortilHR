26ae3e22460641fcb8a5a76cce1c089c92ea3fb7
Thomas Laure <thomaslaure3@gmail.com>
2024-05-07 16:02:44 +0200

    feat: send event to homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use App\Repository\NotificationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,14 +11,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(NotificationRepository $notificationRepository): Response
+    public function index(NotificationRepository $notificationRepository, EventRepository $eventRepository): Response
     {
         $email = 'thomas.laure@fortil.group'; // TODO: get email from AD/Fortil SSO
         $unreadNotifications = $notificationRepository->findBy(['isRead' => false]);
+        $event = $eventRepository->findOneBy([], ['startDate' => 'desc']);
 
         return $this->render('home/index.html.twig', [
             'qrcode_data' => hash('md5', $email.time()),
-            'unread_notifications' => $unreadNotifications
+            'unread_notifications' => $unreadNotifications,
+            'event' => $event
         ]);
     }
 }
