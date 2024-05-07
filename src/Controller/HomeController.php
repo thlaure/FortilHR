d4d85fa7153a9bf5773453158e73cf41ba5995e9
Thomas Laure <thomaslaure3@gmail.com>
2024-05-07 09:36:18 +0200

    feat: get unread notifications

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\NotificationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,12 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(NotificationRepository $notificationRepository): Response
     {
         $email = 'thomas.laure@fortil.group'; // TODO: get email from AD/Fortil SSO
+        $unreadNotifications = $notificationRepository->findBy(['isRead' => false]);
 
         return $this->render('home/index.html.twig', [
-            'qrcode_data' => hash('md5', $email.time())
+            'qrcode_data' => hash('md5', $email.time()),
+            'unread_notifications' => $unreadNotifications
         ]);
     }
 }
