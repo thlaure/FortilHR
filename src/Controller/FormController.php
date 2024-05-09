5130324ed86c7730d9884eac7a147c829979a119
Thomas Laure <thomaslaure3@gmail.com>
2024-05-09 16:36:23 +0200

    feat: list forms

---
+++ src/Controller/FormController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\FormRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FormController extends AbstractController
+{
+    #[Route('/backoffice/form', name: 'app_form_list')]
+    public function list(FormRepository $formRepository): Response
+    {
+        return $this->render('form/index.html.twig', [
+            'forms' => $formRepository->findBy([], ['id' => 'DESC']),
+        ]);
+    }
+}
