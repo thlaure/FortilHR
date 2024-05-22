2e0b9ffbe040624ebe97bcf89eb7b859bb51a407
Thomas Laure <thomaslaure3@gmail.com>
2024-05-22 11:31:19 +0200

    refactor: remove unnecessary checks

--- src/Service/FileChecker.php
+++ src/Service/FileChecker.php
@@ -2,19 +2,17 @@
 
 namespace App\Service;
 
-use App\Constant\Constraint;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class FileChecker
 {
     public function __construct(
-        private string $targetDirectory,
         private LoggerInterface $logger
     ) {
     }
 
-    public function checkExtensionIsAllowed(string $fileName, array $allowedExtensions): bool
+    public function isExtensionValid(string $fileName, array $allowedExtensions): bool
     {
         $extension = pathinfo($fileName, PATHINFO_EXTENSION);
         if (!in_array($extension, $allowedExtensions)) {
@@ -26,7 +24,7 @@ class FileChecker
         return true;
     }
 
-    public function checkMimeTypeIsAllowed(string $fileName, array $allowedMimeTypes): bool
+    public function isMimeTypeValid(string $fileName, array $allowedMimeTypes): bool
     {
         $mimeType = mime_content_type($fileName);
         if (!in_array($mimeType, $allowedMimeTypes)) {
@@ -38,34 +36,14 @@ class FileChecker
         return true;
     }
 
-    public function checkFileExtensionIsForbidden(string $fileName, array $forbiddenFileExtensions): bool
+    public function isSizeValid(UploadedFile $file, int $maxFileSize): bool
     {
-        $fileElements = explode('.', $fileName);
-
-        foreach ($fileElements as $fileElement) {
-            if (in_array($fileElement, $forbiddenFileExtensions) || preg_match($fileElement, Constraint::REGEX_PHP_EXTENSION)) {
-                $this->logger->error('Forbidden extension: ' . $fileElement);
-
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public function checkImageSize(UploadedFile $file): bool
-    {
-        if ($file->getSize() > Constraint::IMAGE_MAX_FILE_SIZE) {
+        if ($file->getSize() > $maxFileSize) {
             $this->logger->error('File too large: ' . $file->getSize());
 
             return false;
         }
 
         return true;
-    }
-
-    public function getTargetDirectory(): string
-    {
-        return $this->targetDirectory;
     }
 }
